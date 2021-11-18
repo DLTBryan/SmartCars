200c05df641925c78a330e27e8497c673747d385
DLTBryan <70251144+DLTBryan@users.noreply.github.com>
2021-11-18 23:21:48 +0100

    rue header and cpp file modified << operator overloaded

--- SmartCars/rue.cpp
+++ SmartCars/rue.cpp
@@ -1,6 +1,8 @@
 #include "rue.h"
 
-void Rue::ajouteNoeud(Noeud* n) {
-    v_noeuds[v_noeuds.size() - 1]->ajouteVoisin(n);
-    v_noeuds.push_back(n);
+void Rue::ajouteNoeud(Noeud* noeud) {
+  /*  v_noeuds[v_noeuds.size() - 1]->ajoutevoisin(n);
+    v_noeuds.push_back(n);*/
+
+    d_noeuds.push_back(noeud);
 }
