6ffe4b35c04b45df74bc7c659da97f3dfe38db92
engenteur <admin@chipmnk.dev>
2021-11-19 16:01:17 +0100

    Quelque chose se dessine

--- SmartCars/SmartCars.h
+++ SmartCars/SmartCars.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include<QPainter>
+#include "noeud.h"
 #include "ui_SmartCars.h"
 
 class SmartCars : public QMainWindow
@@ -8,8 +10,10 @@ class SmartCars : public QMainWindow
     Q_OBJECT
 
 public:
-    SmartCars(QWidget *parent = Q_NULLPTR);
+    SmartCars(std::vector<Noeud*> noeuds, QWidget *parent = Q_NULLPTR);
+    virtual void paintEvent(QPaintEvent* event);
 
 private:
     Ui::SmartCarsClass ui;
+    vector<Noeud*> v_noeuds;
 };
