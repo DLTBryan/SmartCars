c9f8f2ea33b5aa1831260e0e7823c9097de268a0
engenteur <admin@chipmnk.dev>
2021-12-23 00:44:15 +0100

    Supression lignes inutiles pour traçage de la carte

--- enc_temp_folder/2bae82e2d7c11e88c88fa924d1e776/main.cpp
+++ enc_temp_folder/2bae82e2d7c11e88c88fa924d1e776/main.cpp
@@ -7,14 +7,22 @@
 #include <QtWidgets/qscrollarea.h>
 
 #include <iostream>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char* argv[]) {
+    auto start = high_resolution_clock::now();
     osmextract extracteur("map_test.osm");
     extracteur.extraire();
+    std::vector<Rue> rues = extracteur.rues();
+    std::cout << "Size rues : " << rues.size() << std::endl;
     std::vector<Noeud*> noeuds = extracteur.noeuds();
     std::cout << "Size noeuds : " << noeuds.size() << std::endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Temps d'exécution : " << duration.count() << " !" << endl;
     
     QApplication a(argc, argv);
 
